db8fc831061e90513197d8105a2deb37dddda54e
lionel-prats <lionel.prats.c@gmail.com>
2023-04-04 20:46:37 -0300

    seccion 41 - video 399

--- Router.php
+++ Router.php
@@ -2,31 +2,39 @@
 
 namespace MVC;
 
-// debuguear() la puedo usar siempre que desde un archivo que requiera esta clase, requiera a su vez el archivo /includes/funciones.php (si un archivo requiere /includes/app.php, esta requiriendo a su vez /includes/funciones.php)
+// debuguear() la puedo usar acá siempre que desde un archivo que requiera esta clase Router, requiera a su vez el archivo /includes/funciones.php (si un archivo requiere /includes/app.php, esta requiriendo a su vez /includes/funciones.php)
 class Router {
 
     public $rutasGET = [];
     public $rutasPOST = [];
 
+    // esta funcion edita el atributo $this->rutasGET
     public function get($url, $fn) {
+        // $url va a ser una ruta valida de nuestra aplicacion
+        // $fn va a ser un arreglo con el namespace de un controlador y un metodo interno de ese controlador 
         $this->rutasGET[$url] = $fn;
-    }
+    } // -> VIDEO 398
 
     public function comprobarRutas() {
-        
+
         $urlActual = $_SERVER["PATH_INFO"] ?? "/"; // VIDEO 398
         $metodo = $_SERVER["REQUEST_METHOD"]; // VIDEO 398
 
         if($metodo === "GET") {
             $fn = $this->rutasGET[$urlActual] ?? NULL;
+            //array(2) {
+                //[0]=>
+                //string(31) "Controllers\PropiedadController"
+                //[1]=>
+                //string(5) "index"
+            //}
         }
 
         if($fn) {
-            call_user_func($fn, $this); // VIDEO 398
-
+            call_user_func($fn/* , $this */); // VIDEO 398
         } else {
             debuguear("404 NOT FOUND");
         }
 
     }
-}
+} // VIDEO 398
--- public/index.php
+++ public/index.php
@@ -3,13 +3,16 @@
 require_once __DIR__ . "/../includes/app.php";
 
 use MVC\Router; 
+use Controllers\PropiedadController; 
 
 $router = new Router();
 
-$router->get("/", "homeController"); 
-$router->get("/contacto", "contactController");
-$router->get("/nosotros", "aboutController");
-$router->get("/tienda", "marketController");
+//debuguear(PropiedadController::class);
+
+$router->get("/admin", [PropiedadController::class, "index"]); 
+$router->get("/propiedades/crear", [PropiedadController::class, "crear"]);
+$router->get("/propiedades/actualizar", ["Controllers\PropiedadController", "actualizar"]);
 // $router->get() -> VIDEO 398
 
+//debuguear($router->rutasGET);
 $router->comprobarRutas();
