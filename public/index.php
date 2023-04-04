ff088361d575d0dc8bba15926e0e77bd790d4ffa
lionel-prats <lionel.prats.c@gmail.com>
2023-04-04 16:09:32 -0300

    seccion 41 - video 397

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php 
-require_once __DIR__ . "/../includes/app.php";
-use Model\Prueba;
 
-$model = new Prueba();
+require_once __DIR__ . "/../includes/app.php";
+
+use MVC\Router; 
+
+$router = new Router();
+
+debuguear($router);
