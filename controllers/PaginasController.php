8b4f49f31b56d738c6aed8be7bbf63ac640a4548
lionel-prats <lionel.prats.c@gmail.com>
2023-04-06 18:54:46 -0300

    seccion 41 - video 417

---
+++ controllers/PaginasController.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Controllers;
+
+class PaginasController {
+    public static function index() {
+       echo "index de la aplicacion";
+    }
+    public static function nosotros() {
+       echo "nosotros de la aplicacion";
+    }
+    public static function propiedades() {
+       echo "propiedades de la aplicacion";
+    }
+    public static function propiedad() {
+       echo "propiedad de la aplicacion";
+    }
+    public static function blog() {
+       echo "blog de la aplicacion";
+    }
+    public static function entrada() {
+       echo "entrada de la aplicacion";
+    }
+    public static function contacto() {
+       echo "contacto de la aplicacion";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 require_once __DIR__ . "/../includes/app.php";
 
 use MVC\Router; 
-use Controllers\PropiedadController; 
+use Controllers\PaginasController;
 use Controllers\VendedorController; 
+use Controllers\PropiedadController; 
 
 $router = new Router();
 
@@ -12,18 +13,30 @@ $router->get("/admin", [PropiedadController::class, "index"]);
 
 // $router->get() -> VIDEO 398
 
-// CRUD propiedades
+// zona privada - solo usuarios autenticados - CRUD propiedades
 $router->get("/propiedades/crear", [PropiedadController::class, "crear"]);
 $router->post("/propiedades/crear", [PropiedadController::class, "crear"]);
 $router->get("/propiedades/actualizar", ["Controllers\PropiedadController", "actualizar"]);
 $router->post("/propiedades/actualizar", ["Controllers\PropiedadController", "actualizar"]);
 $router->post("/propiedades/eliminar", [PropiedadController::class, "eliminar"]);
 
-// CRUD vendedores
+// zona privada - solo usuarios autenticados - CRUD vendedores
 $router->get("/vendedores/crear", [VendedorController::class, "crear"]);
 $router->post("/vendedores/crear", [VendedorController::class, "crear"]);
 $router->get("/vendedores/actualizar", [VendedorController::class, "actualizar"]);
 $router->post("/vendedores/actualizar", [VendedorController::class, "actualizar"]);
 $router->post("/vendedores/eliminar", [VendedorController::class, "eliminar"]);
 
+// zona publica - todos los usuarios
+$router->get("/", [PaginasController::class, "index"]);
+$router->get("/nosotros", [PaginasController::class, "nosotros"]);
+$router->get("/propiedades", [PaginasController::class, "propiedades"]);
+$router->get("/propiedad", [PaginasController::class, "propiedad"]);
+$router->get("/blog", [PaginasController::class, "blog"]);
+$router->get("/entrada", [PaginasController::class, "entrada"]);
+$router->get("/contacto", [PaginasController::class, "contacto"]);
+$router->post("/contacto", [PaginasController::class, "contacto"]);
+
 $router->comprobarRutas();
+
+//echo "<hr>luego de renderizar una vista, se sigue ejecutando el codigo PHP de este archivo";
